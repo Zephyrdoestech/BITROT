167ce20b54410cb6e90b7c1d0a6944a18fb074b5
Lance Chan <chanlance.school@gmail.com>
2026-04-25 14:13:45 +0800

    coding the platform to go down and up

--- bitrot/Scenes/Levels/level_1.gd
+++ bitrot/Scenes/Levels/level_1.gd
@@ -1,5 +1,38 @@
 extends Node2D
 
+@onready var high_platform = $HighPlatform  # path to your platform node
+
 func _ready() -> void:
 	GameManager.respawn_point = $StartMarker.global_position
-	GameManager.current_level = "res://Scenes/Levels/Level_2.tscn"
+	GameManager.current_level = "res://Scenes/Levels/Level_1.tscn"
+	CodingWindow.option_selected.connect(_on_option_selected)
+
+func _on_option_selected(index: int, is_toggled: bool) -> void:
+	match index:
+		0:
+			if is_toggled:
+				lower_platform()
+			else:
+				raise_platform()
+		1:
+			if is_toggled:
+				print("Option 2 selected")
+				#open_door()
+			else:
+				print("Option 2 selected")
+				#close_door()
+		2:
+			if is_toggled:
+				print("Option 3 selected")
+				#turn_on_light()
+			else:
+				print("Option 3 selected")
+				#turn_off_light()
+
+func lower_platform() -> void:
+	var tween = create_tween()
+	tween.tween_property(high_platform, "position:y", high_platform.position.y + 160, 1.0)
+
+func raise_platform() -> void:
+	var tween = create_tween()
+	tween.tween_property(high_platform, "position:y", high_platform.position.y - 160, 1.0)
--- bitrot/Scenes/Objects/coding_window.gd
+++ bitrot/Scenes/Objects/coding_window.gd
@@ -1,25 +1,23 @@
 extends CanvasLayer
 
+signal option_selected(index: int, is_toggled: bool)
+
+var options_default: Array = []   # original labels
+var options_toggled: Array = []   # opposite labels
+var toggled: Array = [false, false, false]  # current state per option
 var selected: int = 0
-var options: Array = ["Option 1", "Option 2", "Option 3"]
 
 func _ready() -> void:
 	visible = false
-	process_mode = Node.PROCESS_MODE_ALWAYS  # runs even while paused
+	process_mode = Node.PROCESS_MODE_ALWAYS
 
-func open(incoming_options: Array) -> void:
-	options = incoming_options
+func open(defaults: Array, opposites: Array) -> void:
+	options_default = defaults
+	options_toggled = opposites
 	visible = true
 	selected = 0
 	update_labels()
 	update_cursor()
-
-func update_labels() -> void:
-	var option_nodes = $Control/Panel/VBoxContainer.get_children()
-	for i in option_nodes.size():
-		option_nodes[i].text = options[i]
-
-
 
 func close() -> void:
 	visible = false
@@ -34,27 +32,28 @@ func _process(_delta) -> void:
 		update_cursor()
 
 	if Input.is_action_just_pressed("down"):
-		selected = min(options.size() - 1, selected + 1)
+		selected = min(2, selected + 1)
 		update_cursor()
 
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("accept"):
 		choose_option(selected)
+
+func choose_option(index: int) -> void:
+	# flip the toggle state for that option only
+	toggled[index] = !toggled[index]
+	update_labels()
+	update_cursor()
+	emit_signal("option_selected", index, toggled[index])
+	close()
+
+func update_labels() -> void:
+	var option_nodes = $Control/Panel/VBoxContainer.get_children()
+	for i in option_nodes.size():
+		# show toggled label if active, default if not
+		option_nodes[i].text = options_toggled[i] if toggled[i] else options_default[i]
 
 func update_cursor() -> void:
 	var option_nodes = $Control/Panel/VBoxContainer.get_children()
 	for i in option_nodes.size():
 		option_nodes[i].add_theme_color_override("font_color",
 			Color.WHITE if i == selected else Color(0.5, 0.5, 0.5))
-
-func choose_option(index: int) -> void:
-	match index:
-		0:
-			print("chose option 1")
-			# do something
-		1:
-			print("chose option 2")
-			# do something
-		2:
-			print("chose option 3")
-			# do something
-	close()
